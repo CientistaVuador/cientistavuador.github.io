1bd826d439527dc69c996320728dedb7014887d0
CientistaVuador <52110280+CientistaVuador@users.noreply.github.com>
2024-08-25 17:50:38 -0300

    First Commit

---
+++ src/main/java/cientistavuador/articlegenerator/ArticleGenerator.java
@@ -0,0 +1,13 @@
+
+package cientistavuador.articlegenerator;
+
+/**
+ *
+ * @author Cien
+ */
+public class ArticleGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
