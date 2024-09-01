84fc4e556e7651a9fc1ac80fdae4379296cbae3e
CientistaVuador <52110280+CientistaVuador@users.noreply.github.com>
2024-09-01 00:39:49 -0300

    New style

--- src/main/java/cientistavuador/articlegenerator/Article.java
+++ src/main/java/cientistavuador/articlegenerator/Article.java
@@ -40,7 +40,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
